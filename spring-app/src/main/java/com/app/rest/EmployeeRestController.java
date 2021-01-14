df455387868e8ae1bc9af63fc4a8cea75ac0f007
842108771 <8421087771>
2021-01-15 00:41:58 +0530

    Add Docker

---
+++ spring-app/src/main/java/com/app/rest/EmployeeRestController.java
@@ -0,0 +1,16 @@
+package com.app.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeRestController {
+
+	@GetMapping("/show")
+	public String show()
+	{
+		return "Hello";
+	}
+}
